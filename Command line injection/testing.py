f65fcbe6f926011356f6430c64f0513504167056
schoolmosiahvb <168146560+schoolmosiahvb@users.noreply.github.com>
2025-08-19 19:27:06 -0600

    changing code to functions

--- Command line injection/testing.py
+++ Command line injection/testing.py
@@ -3,20 +3,22 @@
 import subprocess
 import optparse
 
-# create the object that can handal user arguments ⬇️
-parser = optparse.OptionParser() 
-# adding options to the object ⬇️⬇️⬇️
-parser.add_option("-i","--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-m","--mac", dest="new_mac", help="New MAC address")
-# Reads what the user has entered to be handled ⬇️⬇️
-(options, arguments) = parser.parse_args()
+def get_arguments():
+    # create the object that can handal user arguments ⬇️
+    parser = optparse.OptionParser() 
+    # adding options to the object ⬇️⬇️⬇️
+    parser.add_option("-i","--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m","--mac", dest="new_mac", help="New MAC address")
+    # Reads what the user has entered to be handled ⬇️⬇️
+    return parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
+def change_mac(interface, new_mac):
+    # for the user to see
+    print(f"[+] Changing MAC address for {interface} to {new_mac}")
+    # runs commands in the terminal
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-print(f"[+] Changing MAC address for {interface} to {new_mac}")
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
-
+(options, arguments) = get_arguments()
+change_mac(options.interface, options.new_mac)
